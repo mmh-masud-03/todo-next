06a7e94fd787639918acf498bdec32c84923f408
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-02-17 22:42:47 +0600

    added task filter

--- src/app/components/ToDoCard.tsx
+++ src/app/components/ToDoCard.tsx
@@ -1,15 +1,30 @@
-import React from "react";
 import { SingleTask } from "./ToDoWrapper";
 interface Props {
   todo: SingleTask;
+  toggleTaskStatus: (id: number) => void;
 }
-function ToDoCard({ todo }: Props) {
+function ToDoCard({ todo, toggleTaskStatus }: Props) {
+  const handleCheckboxChange = () => {
+    toggleTaskStatus(todo.id);
+  };
   return (
     <div className="p-4 rounded-md shadow-md bg-gradient-to-r from-violet-700 to-fuchsia-700">
-      <span className="block text-center text-white text-2xl">
-        {todo?.name}
-      </span>
-      <p className="text-white text-left text-lg my-3">{todo?.description}</p>{" "}
+      <div className="flex flex-row items-baseline mb-2">
+        {" "}
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={handleCheckboxChange}
+          className="text-white"
+        />
+        <span className="block text-center text-white text-2xl ml-6">
+          {todo?.name}
+        </span>
+      </div>
+      <hr />
+      <p className="text-white text-left text-lg my-3">
+        {todo?.description}
+      </p>{" "}
     </div>
   );
 }
--- src/app/components/ToDoWrapper.tsx
+++ src/app/components/ToDoWrapper.tsx
@@ -2,30 +2,64 @@
 import React, { useState } from "react";
 import ToDoForm from "./ToDoForm";
 import ToDoCard from "./ToDoCard";
+
 export interface SingleTask {
   id: number;
   name: string;
   description: string;
   completed: boolean;
 }
+
 function ToDoWrapper() {
   const [showForm, setShowForm] = useState(false);
   const [todos, setTodos] = useState<SingleTask[]>([]);
+  const [filter, setFilter] = useState<string>("all");
+
   const addTodo = (newTask: SingleTask) => {
     setTodos([...todos, newTask]);
   };
-  const handelFormOpen = () => {
+
+  const handleFormOpen = () => {
     setShowForm(!showForm);
   };
+
+  const toggleTaskStatus = (id: number) => {
+    const toggledTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(toggledTodos);
+  };
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilter(e.target.value);
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === "all") {
+      return true;
+    } else if (filter === "completed") {
+      return todo.completed;
+    } else {
+      return !todo.completed;
+    }
+  });
+
   return (
     <div>
       <span className="text-4xl text-white font-bold block mx-auto py-3">
         ToDo Application
       </span>
-      <div className=" flex flex-row items-center justify-around w-1/2 mt-6 mx-auto">
+      <div className="flex flex-row items-center justify-around w-1/2 mt-6 mx-auto">
         <div>
           <span className="text-white px-2 text-xl">Filter By:</span>
-          <select className="bg-inherit border p-1 rounded text-black">
+          <select
+            value={filter}
+            onChange={handleFilterChange}
+            className="bg-inherit border p-1 rounded text-black"
+          >
             <option value="all">All</option>
             <option value="completed">Completed</option>
             <option value="active">Active</option>
@@ -33,18 +67,22 @@ function ToDoWrapper() {
         </div>
         <div>
           <button
-            className="border rounded bg-blue-600 py-1 px-2  text-white text-xl"
-            onClick={handelFormOpen}
+            className="border rounded bg-blue-600 py-1 px-2 text-white text-xl"
+            onClick={handleFormOpen}
           >
             Create Task
           </button>
         </div>
       </div>
-      {showForm && <ToDoForm onCancel={handelFormOpen} addTodo={addTodo} />}
+      {showForm && <ToDoForm onCancel={handleFormOpen} addTodo={addTodo} />}
       <div className="grid grid-cols-4 gap-2 w-3/4 mt-20 mx-auto">
-        {todos.map((todo) => (
-          <ToDoCard key={todo.id} todo={todo} />
-        ))}{" "}
+        {filteredTodos.map((todo) => (
+          <ToDoCard
+            key={todo.id}
+            todo={todo}
+            toggleTaskStatus={toggleTaskStatus}
+          />
+        ))}
       </div>
     </div>
   );
